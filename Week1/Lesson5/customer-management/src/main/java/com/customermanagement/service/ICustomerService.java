7e0ce305e55260e9da5f37d85341ae728a54a35c
Your Namelesonnhat <sonnhatbest@gmail.com>
2025-04-23 11:04:15 +0700

    add customer management

---
+++ Week1/Lesson5/customer-management/src/main/java/com/customermanagement/service/ICustomerService.java
@@ -0,0 +1,15 @@
+package com.customermanagement.service;
+
+import com.customermanagement.model.Customer;
+
+import java.util.List;
+
+public interface ICustomerService {
+    List<Customer> findAll();
+
+    void save(Customer customer);
+
+    Customer findById(int id);
+
+    void remove(int id);
+}
